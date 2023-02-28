1222472ade56865aa7f9480bd32839fe24b53af4
Igor Bogomolov <spaik111@gmail.com>
2023-02-28 10:22:46 +0300

    ДоделанИзменены модели, апи

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -36,28 +36,46 @@ class User(AbstractUser):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    name = models.CharField('Категория', max_length=256, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True)
+    name = models.CharField('Жанр', max_length=256, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=256)
-    year = models.PositiveIntegerField()
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField('Название', max_length=256)
+    year = models.PositiveIntegerField('Год выпуска')
+    description = models.TextField('Описание', blank=True, null=True)
     genre = models.ManyToManyField(Genre)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    rating = models.PositiveIntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='category',
+        verbose_name='Категория',
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def __str__(self):
         return self.name
