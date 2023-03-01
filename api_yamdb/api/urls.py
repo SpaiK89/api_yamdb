88eaa60f3a29544ec1cd114f4c5dde1808e33ec7
Igor Bogomolov <spaik111@gmail.com>
2023-03-01 15:18:13 +0300

    Добавлены api/urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,6 +7,14 @@ router_v1.register('categories', views.CategoryViewSet, basename='categories')
 router_v1.register('genres', views.GenreViewSet, basename='genres')
 router_v1.register('titles', views.GenreViewSet, basename='titles')
 router_v1.register('users', views.UserViewSet, basename='users')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    views.ReviewViewSet, basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    views.CommentsViewSet, basename='comments',
+)
 
 authentication = [
     path("signup/", views.get_signup),
