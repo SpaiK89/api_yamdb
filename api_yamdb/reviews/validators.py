d871d716fd1673241b771fb95af9ec7b21ab5bda
Игорь <116277615+SpaiK89@users.noreply.github.com>
2023-03-03 22:53:21 +0300

    Update validators.py
    Исправлена орфографическая ошибка

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -7,5 +7,5 @@ def year_validator(value):
     year = dt.datetime.now().year
     if year < value:
         raise ValidationError(f'{value} - некорректный год издания'
-                              f'произведения! Укажите год неболее {year}')
+                              f'произведения! Укажите год не более {year}')
     return value
